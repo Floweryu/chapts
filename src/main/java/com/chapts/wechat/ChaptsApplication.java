2e5fa366aaedca4e61a6714289aa98d41f4849e1
zhangjunfeng <im.zhangjunfeng@qq.com>
2023-11-06 20:44:46 +0800

    first commit

---
+++ src/main/java/com/chapts/wechat/ChaptsApplication.java
@@ -0,0 +1,13 @@
+package com.chapts.wechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaptsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChaptsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chapts/wechat/ChaptsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chapts.wechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaptsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
